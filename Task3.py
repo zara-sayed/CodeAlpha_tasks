57f0fe3bc8ad4769caa36b06f2389b3645c953b9
zara-sayed <HUMAIRA.SAYED.5125307@ves.ac.in>
2026-06-13 16:55:26 +0530

    Add files via upload

---
+++ Task3.py
@@ -0,0 +1,28 @@
+# Basic Chatbot Program
+
+def chatbot():
+
+    print("Welcome to Simple Chatbot")
+    print("Type 'bye' to exit")
+
+    while True:
+
+        message = input("You: ").lower()
+
+        if message == "hello":
+            print("Bot: Hi!")
+
+        elif message == "how are you ?":
+            print("Bot: I'm fine, thanks!")
+
+        elif message == "Khana hua ?":
+            print("Bot: Haa!")
+
+        elif message == "bye":
+            print("Bot: Goodbye!")
+            break
+
+        else:
+            print("Bot: Sorry, I don't understand.")
+
+chatbot()
